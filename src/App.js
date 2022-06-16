12e6e32d4970da00390e49069eed9570f307deae
George Owusu <georgeowusu@georges-mbp.myfiosgateway.com>
2022-06-16 01:08:11 -0400

    created Search input

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import * as React from 'react'; import logo from './logo.svg'; import './App.css';
-function App() { return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-        </a>
-      </header>
-</div> );
+import * as React from 'react';
+
+function getTitle(title) {
+  return title;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
 }
 
 export default App;
